55ed7973c42278344cfc04d78e933128ab700c81
BeanPaste <1656425255@qq.com>
2021-10-07 23:51:35 +0800

    add some details

--- MainPlugin.cs
+++ MainPlugin.cs
@@ -1,12 +1,57 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI;
 
 namespace TileCommands
 {
-    public class Class1
+    public class MainPlugin : TerrariaPlugin
     {
+        public MainPlugin(Main game) : base(game)
+        {
+        }
+
+        public override string Name => "TileCommands";
+
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+
+        public override string Author => "豆沙";
+
+        public override string Description => "当玩家走到某个方块上时执行指令";
+
+        public override void Initialize()
+        {
+            ServerApi.Hooks.GamePostInitialize.Register(this,OnPostInitialize);
+            GetDataHandlers.PlayerUpdate += OnPlayerUpdate;
+        }
+        private void OnPlayerUpdate(object sender,GetDataHandlers.PlayerUpdateEventArgs args) 
+        {
+            
+        
+        
+        }
+        private void OnPostInitialize(EventArgs args)
+        {
+            Commands.ChatCommands.Add(new Command("tc.admin",TC,"tc"));
+        }
+
+        private void TC(CommandArgs args)
+        {
+           
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+
+            }
+            base.Dispose(disposing);
+        }
     }
 }
---
+++ Utils.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TileCommands
+{
+    public static class Utils
+    {
+        //to do
+        public static Point GetPlayerFeetPoint(Point position) {
+            return Point.Zero;
+        }
+    }
+}
